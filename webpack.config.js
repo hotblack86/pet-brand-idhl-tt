5029d629026a4ceef65419d6cc014893e7ad8df2
James Hotblack <james.c.hotblack@gmail.com>
2025-08-17 16:28:51 +0100

    Added Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { 
+          loader: 'babel-loader', 
+          options: { presets: ['@babel/preset-env'] } 
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 
+          'css-loader',                
+          'sass-loader'                
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
+  ]
+};
